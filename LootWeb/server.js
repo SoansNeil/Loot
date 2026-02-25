c3436c3a6b114113807a809dfaffd0e9d48d801e
Neil Soans <nellysoans8@gmail.com>
2026-02-24 19:54:16 -0600

    fixed issues with server

--- LootWeb/server.js
+++ LootWeb/server.js
@@ -30,8 +30,8 @@ app.post('/createUser-form', (req, res) => {
   .update(req.body.password)
   .digest('hex');
   
-  const sql = 'INSERT INTO SUBSCRIBER_ACCOUNT (firstName, lastName, username, password, birthday, email, phoneNumber) VALUES (?, ?, ?, ?, ?, ?, ?)';
-  db.query(sql, [firstName, lastName, username, req.body.password, birthday, email, phoneNumber], (err, result) => {
+  const sql = 'INSERT INTO SUBSCRIBER_ACCOUNT (FName, LName, Username, Password, Birthday, Email, PhoneNumber) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  db.query(sql, [firstName, lastName, username, hashPassword, birthday, email, phoneNumber], (err, result) => {
     if (err) {
       console.error('Error inserting user:', err);
       res.status(500).send('Error inserting user');
