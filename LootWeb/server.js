e99c300c9b6205589801c168c0f3267a539bf77b
Neil Soans <nellysoans8@gmail.com>
2026-03-02 15:09:18 -0600

    added server logic for user based access, created html for user dashboard, updated script in employee login to send employee to employeeDashboard.html

--- LootWeb/server.js
+++ LootWeb/server.js
@@ -1,5 +1,6 @@
 //NEED FORM FROM HTML FILES TO TEST CONNECTIONS
 const express = require('express');
+const session = require('express-session');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
@@ -8,6 +9,11 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
+app.use(session({
+  secret: 'loot-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -16,7 +22,43 @@ const db = mysql.createConnection({
   database: 'LootDB'
 });
 const crypto = require('crypto'); // For hashing passwords and sensitive data
+//employeeLogin
+app.post('/employeeLogin', (req,res) =>{
+  console.log("Employee login POST hit:", req.body);
+  const username = req.body.eUsername;
+  const hashPassword = crypto
+    .createHash('sha256')
+    .update(req.body.ePassword.trim())
+    .digest('hex')
 
+  const sql = 'SELECT * FROM Employee WHERE eUsername = ?';
+  db.query(sql, [username], (err,result) =>{
+    if (err) return res.status(500).send("Server Error");
+    if (result.length === 0){
+      return res.status(401).send("Employee Not Found");
+    }
+    const employee = result[0];
+    if(employee.ePassword !== hashPassword) 
+      return res.status(401).send("Incorrect Password");
+
+    req.session.user = {
+      id: employee.EmployeeID,
+      role: "employee"
+    };
+    res.send("Success");
+  });
+});
+app.get('/employeeDashboard', (req, res) => {
+  if (!req.session.user) {
+    return res.send("Please login first.");
+  }
+
+  if (req.session.user.role !== "employee") {
+    return res.send("Access denied. Employees only.");
+  }
+
+  res.sendFile(__dirname + '/employeeDashboard.html');
+});
 //Route to add new users to database
 app.post('/createUser-form', (req, res) => {
   const firstName = req.body.firstName;
@@ -58,7 +100,6 @@ const sql = 'INSERT INTO EXTERNAL_ACCOUNT (bankName, accountType) VALUES (?, ?)'
     res.send('External account connected successfully!');
   });
 });
-//Create Route for employee login and employee dashboard (can be separate routes)
 //Server checks
 app.use((req, res) => {
   res.status(404).send('Not Found');
@@ -67,3 +108,13 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+//functions
+function authorize(allowedRoles){
+  return (req,res,next) => {
+    if(!req.session.user) return res.status(401).send("Please Log In");
+    if(!allowedRoles.includes(req.session.user.role)){
+      return res.status(401).send("Access Denied");
+    }
+    next();
+  };
+}
