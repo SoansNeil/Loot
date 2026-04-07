e7b344260b91280a3559aa9d2bc44b87231fc8e1
Neil Soans <nellysoans8@gmail.com>
2026-04-06 22:35:07 -0500

    default port is now 8080

--- LootWeb/server.js
+++ LootWeb/server.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
