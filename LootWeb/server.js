a73870adb7d42a52af30dff3bc61dbb1bc744121
Neil Soans <nellysoans8@gmail.com>
2026-04-13 19:27:28 -0500

    updated db connection to use pooling

--- LootWeb/server.js
+++ LootWeb/server.js
@@ -17,21 +17,23 @@ app.use(express.static(__dirname));
 app.use(cookieParser());
 
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("Connection Failed:", err);
-  }
-  else {
+  } else {
     console.log("Connected to LootDB");
+    connection.release();
   }
 });
 
