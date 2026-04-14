a3729e6dabaddf757a91f5c93ea841af3a507518
Neil Soans <nellysoans8@gmail.com>
2026-04-13 19:41:15 -0500

    added nav.js in order to pass subscriberID throughout every page

---
+++ LootWeb/assets/js/nav.js
@@ -0,0 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const subscriberID = urlParams.get('subscriberID');
+
+const goToExAccounts = () => window.location.assign(`ExAccounts.html?subscriberID=${subscriberID}`);
+const goToTransfer   = () => window.location.assign(`MT-Dashboard.html?subscriberID=${subscriberID}`);
+const goToBudget     = () => window.location.assign(`budget.html?subscriberID=${subscriberID}`);
+const goToFamily     = () => window.location.assign(`Fam-dash.html?subscriberID=${subscriberID}`);
+const goToSummary    = () => window.location.assign(`sum_dashboard.html?subscriberID=${subscriberID}`);
+const goToDashboard  = () => window.location.assign(`Dashboard.html?subscriberID=${subscriberID}`);
+const goToExternalAccounts = () => window.location.assign(`ExAccounts.html?subscriberID=${subscriberID}`);
